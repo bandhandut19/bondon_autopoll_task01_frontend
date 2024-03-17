dbc4d1baf2a884ec0b98e7713a086f1f9ac441fe
bandhandut19 <bondon21081@gmail.com>
2024-03-17 18:14:10 +0600

    fixed overflow issue in adminpanel

--- src/Components/AdminPanel/SingleUser.jsx
+++ src/Components/AdminPanel/SingleUser.jsx
@@ -2,9 +2,9 @@
 const SingleUser = ({user}) => {
     const {name,email} = user
     return (
-        <div className="border-2 border-orange-500 rounded-md px-5 py-2">
-            <h1><span className="font-bold">User Name: <br /> </span> {name}</h1>
-            <h1><span className="font-bold">User Email: <br /></span>{email}</h1>
+        <div className="border-2 border-orange-500 rounded-md px-5 py-2 flex flex-col overflow-auto">
+            <h1><span className="font-bold mb-2">User Name: </span> {name}</h1>
+            <h1><span className="font-bold">User Email: </span>{email}</h1>
         </div>
     );
 };
