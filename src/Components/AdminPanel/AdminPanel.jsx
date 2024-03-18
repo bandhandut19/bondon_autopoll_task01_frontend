74f5fce7f6b7e047a8a3a2ed5423e20b372806ae
bandhandut19 <bondon21081@gmail.com>
2024-03-18 09:48:01 +0600

    getting current user date of register

--- src/Components/AdminPanel/AdminPanel.jsx
+++ src/Components/AdminPanel/AdminPanel.jsx
@@ -3,8 +3,18 @@ import { useEffect, useState } from "react";
 import SingleUser from "./SingleUser";
 import { userEmail } from "../Login/Login";
 const AdminPanel = () => {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]); /* all users */
+    const [userDate, setUserDate] = useState([]); /* current user date */
     const [currentUserData, setCurrentUserData] = useState([]); /*  this one holds current user */
+
+    // current user data //
+    useEffect(() => {
+        if (userEmail) {
+            const filteredUser = users.filter(user => user.email === userEmail);
+            setCurrentUserData(filteredUser);
+        }
+    }, [userEmail, users]);
+
     useEffect(() => {
         axios.get('http://localhost:5000/users')
             .then(res => {
@@ -15,12 +25,21 @@ const AdminPanel = () => {
             });
     }, []);
 
+    /////////////////////////////
+
+    // current user date
+    
     useEffect(() => {
-        if (userEmail) {
+        if (currentUserData) {
             const filteredUser = users.filter(user => user.email === userEmail);
-            setCurrentUserData(filteredUser);
+            setUserDate(filteredUser[0]?.date)
         }
-    }, [userEmail, users]);
+    }, [userEmail, users,currentUserData]);
+    
+
+
+
+
 
 
 
@@ -29,6 +48,7 @@ const AdminPanel = () => {
         <div className="w-4/5 mx-auto mt-10 bg-orange-100 px-5 py-5">
 
             <h1 className="text-center text-2xl font-bold mb-5">Welcome <span className="text-green-500">{currentUserData[0]?.name}</span> !</h1>
+            
             <h1 className="font-bold text-2xl text-center">Total users under you are: <span className="text-green-600 text-2xl"> { currentUserData?.length == 0 ? <h1>0</h1>: users.length} </span></h1>
             <div className="grid grid-cols-3 gap-5 mt-5">
                 {
