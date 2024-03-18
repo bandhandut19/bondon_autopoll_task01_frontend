175272b09eaa068fc3f6d228dd8dc6247b3bce6f
bandhandut19 <bondon21081@gmail.com>
2024-03-18 11:45:54 +0600

    implemented one user can click once

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,25 +1,21 @@
 import { userEmail } from "../Login/Login";
 import { useNavigate } from "react-router-dom";
 
-
-export let clickedUsers = []
+export let clickedUsers = [];
 
 const Home = () => {
+    const navigate = useNavigate();
 
-    const navigate = useNavigate()
     const handleClick = () => {
-        if (userEmail != null) {
-
-            clickedUsers.push(userEmail)
+        if (userEmail && !clickedUsers.includes(userEmail)) {
+            clickedUsers.push(userEmail);
         }
-        navigate('/adminpanel')
-    }
+        navigate('/adminpanel');
+    };
+
     return (
         <div className="w-4/5 mx-auto mt-20 flex items-center justify-center">
-
-
             <button onClick={handleClick} className="btn btn-outline btn-success">Auto Poll</button>
-
         </div>
     );
 };
